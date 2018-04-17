081d73a2e0a0365110b23d7bb2f36ad34f22511d
Aida7 <33822250+Aida7@users.noreply.github.com>
2018-04-17 21:58:37 +0600

    DB Shop

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Create create = new Create();
+
+            create.CreateTableProducts();
+            create.CreateTableEmployees();
+            create.CreateTableCustomers();
+            create.CreateTableOrders();
+            create.CreateTableOrderDetails();
+        }
+    }
+}
